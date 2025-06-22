fc3002d18605f2164ebdd699fc8d61bacffa8fc5
Bhuvan Raj <163994129+bhuvan-raj@users.noreply.github.com>
2025-06-22 21:04:30 +0530

    Update main.tf

--- Local-Terraform-Modules/main.tf
+++ Local-Terraform-Modules/main.tf
@@ -6,13 +6,3 @@ provider "aws" {
 module "vpc" {
   source = "./modules/vpc"
 }
-
-# Remote module for subnets (Terraform Registry)
-module "subnets" {
-  source  = "terraform-aws-modules/subnets/aws"
-  version = "2.7.0"
-
-  vpc_id             = module.vpc.vpc_id
-  availability_zones = ["us-east-1a", "us-east-1b"]
-  cidr_blocks        = ["10.0.1.0/24", "10.0.2.0/24"]
-}
